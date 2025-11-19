058d6939e24cae5ac7f65971f10f36cfea810a14
Nancy <nnguyen464@gatech.edu>
2025-11-18 23:46:21 -0500

    Initial Commit

---
+++ Productivity/ProductivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityApp.swift
+//  Productivity
+//
+//  Created by Student_User on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
